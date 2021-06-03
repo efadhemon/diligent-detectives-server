69019edfaa5d3350da23e6fbb163a384d90b0d1f
Mohammad Emon <mohammademon551@gmail.com>
2021-06-03 13:00:23 +0600

    Edit testimonial api fix

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ client.connect(err => {
 
         app.patch('/editTestimonial/:id', (req, res) => {
             testimonialsCollection.updateOne({ _id: ObjectId(req.params.id) }, {
-                $set: { name: req.body.name, cost: req.body.from, description: req.body.quote}
+                $set: { name: req.body.name, from: req.body.from, quote: req.body.quote}
             })
                 .then(result => {
                     res.send(result.modifiedCount > 0)
